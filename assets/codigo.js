80fc989a784464e22c0a399993f67a0cb36f640d
Daniel <torresvalenzueladanielarturo@gmail.com>
2024-08-16 09:06:59 -0500

    ultimas actualizaciones

--- assets/codigo.js
+++ assets/codigo.js
@@ -1,7 +1,6 @@
-console.log('aprendiendo a invocar codigo js desde HTML')
+console.log("aprendiendo a invocar codigo js desde HTML");
 
-let primernumer= 2; 
-let segundonumero=4;
-let resultado = primernumer + segundonumero
-console.log(resultado)
-  
+let primernumer = 2;
+let segundonumero = 4;
+let resultado = primernumer + segundonumero;
+console.log(resultado);
